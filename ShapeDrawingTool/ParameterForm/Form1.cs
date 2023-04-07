3f62c053e6baef1aafcc46c61a3c2e3b1c637998
Kavinda Udara <98000159+2000kavinda@users.noreply.github.com>
2023-04-08 01:29:22 +0530

    upload circle parameter getting and calculate dots count

---
+++ ShapeDrawingTool/ParameterForm/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ParameterForm.ServiceReference1;
+
+namespace ParameterForm
+{
+    public partial class Form1 : Form
+    {
+        WebService1SoapClient obj;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(textBox1.Text);
+            int b = Convert.ToInt32(textBox2.Text);
+            double c = obj.Circle(a, b);
+            MessageBox.Show("Dots count = " + c);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            obj = new WebService1SoapClient();
+        }
+    }
+}
