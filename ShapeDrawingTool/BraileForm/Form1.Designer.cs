77fed1a873625035fdcfcec3005d28c023556fc1
Kavinda Udara <98000159+2000kavinda@users.noreply.github.com>
2023-04-10 16:58:39 +0530

    text convert to braile in same form

---
+++ ShapeDrawingTool/BraileForm/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace BraileForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InputTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.OutputTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // InputTextBox
+            // 
+            this.InputTextBox.Location = new System.Drawing.Point(158, 114);
+            this.InputTextBox.Name = "InputTextBox";
+            this.InputTextBox.Size = new System.Drawing.Size(100, 22);
+            this.InputTextBox.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(306, 211);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Convert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // OutputTextBox
+            // 
+            this.OutputTextBox.Location = new System.Drawing.Point(428, 114);
+            this.OutputTextBox.Name = "OutputTextBox";
+            this.OutputTextBox.Size = new System.Drawing.Size(100, 22);
+            this.OutputTextBox.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.OutputTextBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.InputTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox InputTextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox OutputTextBox;
+    }
+}
+
--- ShapeDrawingTool/ShapeDrawingTool/WebService1.asmx.cs
+++ ShapeDrawingTool/ShapeDrawingTool/WebService1.asmx.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Services;
 
@@ -25,5 +26,63 @@ namespace ShapeDrawingTool
             double c = p / (g * 2);
             return Math.Round(c);
         }
+        [WebMethod]
+        public String abc(string inputText)
+        {
+            // Create a dictionary to map regular letters to their corresponding braille letters
+            Dictionary<char, string> brailleMap = new Dictionary<char, string>()
+            {
+                {'a', "⠇"},
+                {'b', "⠃"},
+                {'c', "⠉"},
+                {'d', "⠙"},
+                {'e', "⠑"},
+                {'f', "⠋"},
+                {'g', "⠛"},
+                {'h', "⠓"},
+                {'i', "⠊"},
+                {'j', "⠚"},
+                {'k', "⠅"},
+                {'l', "⠇"},
+                {'m', "⠍"},
+                {'n', "⠝"},
+                {'o', "⠕"},
+                {'p', "⠏"},
+                {'q', "⠟"},
+                {'r', "⠗"},
+                {'s', "⠎"},
+                {'t', "⠞"},
+                {'u', "⠥"},
+                {'v', "⠧"},
+                {'w', "⠺"},
+                {'x', "⠭"},
+                {'y', "⠽"},
+                {'z', "⠵"}
+            };
+            String k = " ";
+            // Create a StringBuilder to store the output braille letters
+            //StringBuilder outputBuilder = new StringBuilder();
+
+            // Convert each letter of the input text to its corresponding braille letter
+            foreach (char c in inputText)
+            {
+                // Check if the letter is in the dictionary
+                if (brailleMap.ContainsKey(c))
+                {
+                    // If the letter is in the dictionary, add its braille letter to the output
+                    //outputBuilder.Append(brailleMap[c]);
+                    k = k + brailleMap[c];
+                }
+                else
+                {
+                    // If the letter is not in the dictionary, add a space to the output
+                    //outputBuilder.Append(" ");
+                    k = k + " ";
+                }
+
+            }
+            return k;
+        }
+
     }
 }
